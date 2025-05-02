17abc6a0b515ef159671b041cfbf3b2b075aa80f
Priyanka Rani <161093822+Priyankarani29@users.noreply.github.com>
2025-05-02 14:32:33 -0400

    Add files via upload

--- spotifyapp.py
+++ spotifyapp.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1mfblxzPUSDusSfc6l1zafZ7HAO9zO-DY
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import json
